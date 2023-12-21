fdae5fd67f21be12c7e6164100306f1c077a5b37
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-12-21 02:31:00 +0200

    Build TabletLayout

--- lib/widgets/layout_builder/mobile_layout.dart
+++ lib/widgets/layout_builder/mobile_layout.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_adaptive/screens/layout_builder/item_details_screen.dart';
+import 'package:responsive_adaptive/widgets/layout_builder/item.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({
@@ -11,35 +11,7 @@ class MobileLayout extends StatelessWidget {
     return ListView.builder(
       itemCount: 10,
       itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ItemDetailsScreen(number: index),
-              ),
-            );
-          },
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            color: index % 2 == 0 ? Colors.red : Colors.blue,
-            child: ListTile(
-              title: const Text(
-                'Item',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-              subtitle: Text(
-                '${index + 1}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
-              ),
-            ),
-          ),
-        );
+        return Item(index: index);
       },
     );
   }
